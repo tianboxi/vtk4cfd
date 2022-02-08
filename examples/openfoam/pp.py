b7eb437bfe56eb0c871a2bdc07cc792206d44f72
Raye Xie <Tianboxi@usc.edu>
2022-02-08 12:20:09 -0800

    add new example for openfoam

--- examples/openfoam/pp.py
+++ examples/openfoam/pp.py
@@ -31,4 +31,31 @@ solfile = './tiny_1933.vtk'
 options ={
   'solvarnames':{'P':'p', 'V':'U'}
 }
-sol_obj = Grid(solfile, 'VTK', options=options, plotoptions={})
+sol = Grid(solfile, 'VTK', options=options, plotoptions={})
+
+plotvarnames = ['P','U']
+nmercontour, merc, mercf = len(plotvarnames), [], []
+for ii in range(nmercontour):
+   fig, thisax = sol.makePlot()
+   merc.append(thisax)
+   mercf.append(fig)
+
+#sol.computeVar(['V/Vinf','Cpt','Cp'])
+zloc = -0.5 
+sol.makeSlice([0,0,zloc], [0,0,-1.0],slicename='midspan',view='-z')
+sol.makeSlice([0,0,zloc], [0,0,-1.0],slicename='midspan_quad',view='-z', triangulize=False)
+sline_seeds = np.linspace([-1.0, -0.3, zloc], [-1.0, 0.3, zloc], 100)
+sol.plotStreamlineMultiBlk('U', sline_seeds, surface='midspan_quad', x_range=[-1.0, 1.0], idir='forward', slname='midspan')
+
+sol.plotContour('p', surface='midspan', ax=merc[0])
+#,sol.plotContour('Cp', surface='midspan', ax=merc[2], clevels=[-6.0, 6.0])
+
+#for axes in merc:
+sol.plotSavedStreamline('midspan', merc[0])
+
+dpi = 300.0
+#for ifig, axes in enumerate(merc):
+merc[0].set_xlim([-0.5, 1.5])
+merc[0].set_ylim([-0.3, 0.3])
+mercf[0].savefig('contours/'+plotvarnames[0]+'.png', dpi=dpi, bbox_inches='tight')
+
